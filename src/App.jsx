3bb14183936daacfd01e0b3a325c3876878d6f20
maithenkumar <104820167+maithenkumar@users.noreply.github.com>
2024-02-17 02:58:40 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import MyComponent from "./demo";
+
+export default function App() {
+  return (
+    <main>
+      React Vite + Replit
+      <MyComponent />
+    </main>
+  );
+}
